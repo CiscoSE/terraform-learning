8353644cf85d5ca0a610c5166c6619d4fd7bf110
oldguardmd <oldguard@msn.com>
2021-06-01 14:28:21 -0400

    ACI Filter and Contract Examples

---
+++ 202106-Demo/ACI/aci-contracts.tf
@@ -0,0 +1,19 @@
+locals{
+    stateful = "no"
+}
+
+resource "aci_contract" "IPERF" {
+    tenant_dn = local.tenant_id
+    name      = "Demo1-Demo2-outbound"
+    scope     = "tenant"
+    filter {
+        filter_name = "allow_icmp"
+        description = "Demo ICMP Filter"
+        filter_entry {
+            filter_entry_name = "icmp"
+            ether_t     = "ip"
+            prot     = "icmp"
+            stateful = local.stateful
+        }
+    }
+}
---
+++ 202106-Demo/ACI/aci-filters.tf
@@ -0,0 +1,41 @@
+
+resource "aci_filter" "allow_web" {
+    tenant_dn = "${local.tenant_id}"
+    name      ="allow_web"
+}
+
+resource "aci_filter_entry" "http" {
+    name        = "http"
+    filter_dn   = "${aci_filter.allow_web.id}"
+    ether_t     = "ipv4"
+    prot        = "tcp"
+    d_from_port = "80"
+    d_to_port   = "80"
+    stateful = local.stateful
+}
+
+resource "aci_filter_entry" "https" {
+    name        = "https"
+    filter_dn   = "${aci_filter.allow_web.id}"
+    ether_t     = "ipv4"
+    prot        = "tcp"
+    d_from_port = "443"
+    d_to_port   = "443"
+    stateful = local.stateful
+}
+
+resource "aci_filter" "allow_iperf"{
+    name       = "allow_iperf"
+    tenant_dn  = "${local.tenant_id}"
+}
+
+resource "aci_filter_entry" "iperf" {
+    name        = "iperf"
+    filter_dn   = "${aci_filter.allow_iperf.id}"
+    ether_t     = "ipv4"
+    prot        = "tcp"
+    d_from_port = "5201"
+    d_to_port   = "5201"
+    stateful    = local.stateful
+}
+
