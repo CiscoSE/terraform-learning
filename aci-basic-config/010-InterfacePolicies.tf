1c8273bb1b07d9d221d7634c75d5eaa61d1beff4
oldguardmd <oldguard@msn.com>
2021-08-19 12:14:38 +0000

    First Group of Interface Policies

---
+++ aci-basic-config/010-InterfacePolicies.tf
@@ -0,0 +1,52 @@
+
+locals {
+  CDP_enable_name     = "CDP-Enable"
+  CDP_disable_name    = "CDP-Disable"
+  LLDP_enable_name    = "LLDP-Enable"
+  LLDP_disable_name   = "LLDP-disable"
+  LACP_active_name    = "LACP-Active"
+  LACP_active_ns_name = "LACP-Active-No-Suspend" 
+  LACP_on_name        = "LACP-On"
+}
+
+resource "aci_cdp_interface_policy" "CDP_enable" {
+  name        = local.CDP_enable_name
+  admin_st    = "enabled"
+  description = "Enables CDP on an Interface when applied to an interface group"
+}
+
+resource "aci_cdp_interface_policy" "CDP_disable" {
+  name        = local.CDP_disable_name
+  admin_st    = "disabled"
+  description = "Disables CDP on an Interface when applied to an interface group"
+}
+
+resource "aci_lldp_interface_policy" "LLDP_enable" {
+  name        = local.LLDP_enable_name
+  admin_rx_st = "enabled"
+  admin_tx_st = "enabled"
+  description = "Enables LLDP on an Interface when applied to an interface group"
+}
+
+resource "aci_lldp_interface_policy" "LLDP_disable" {
+  name        = local.LLDP_disable_name
+  admin_rx_st = "disabled"
+  admin_tx_st = "disabled"
+  description = "Disables LLDP on an Interface when applied to an interface group"
+}
+
+resource "aci_lacp_policy" "LACP_active" {
+  name        = local.LACP_active_name
+  ctrl        = ["susp-individual", "load-defer", "graceful-conv"]
+  max_links   = "16"
+  min_links   = "1"
+  mode        = "active"
+}
+
+resource "aci_lacp_policy" "LACP_activei_ns" {
+  name        = local.LACP_active_ns_name
+  ctrl        = ["load-defer", "graceful-conv"]
+  max_links   = "16"
+  min_links   = "1"
+  mode        = "active"
+}
