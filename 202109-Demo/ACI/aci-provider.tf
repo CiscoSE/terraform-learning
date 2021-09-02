07f35b4a7fd783912b88498a8d4d71197ad66a1e
oldguardmd <oldguard@msn.com>
2021-09-02 20:40:58 +0000

    Turned off Backups while woring on the code

--- 202109-Demo/ACI/aci-provider.tf
+++ 202109-Demo/ACI/aci-provider.tf
@@ -8,30 +8,15 @@ terraform {
   }
 }
 
-variable "apic" {
-  default     = ""
-  description = "IP or FQDN of the APIC"
-}
-
-variable "cert_name" {
-  default     = ""
-  description = "This is the name of the certificate as configured in the APIC GUI under the users AAA configuration"
-}
-
-variable "private_key" {
-  default     = ""
-  description = "The location of the private key file that maps to the above named certificate."
-  /*
-  It is really important that you understand the relationship between the private key and the certifiate name.
-  The public key is copied into the AAA configuration for a user and is given a name. The private key
-  must be accessible to terraform so that it can decrypt information received from the APIC, which is encrypted
-  using the public key associated with the account.
-  */
-}
-
-variable "user_name" {
-  default     = ""
-  description = "The user account used to log into the APIC"
+variable apic_auth {
+  type = (
+    {
+      apic        = string
+      cert_name   = string
+      private_key = string
+      user_name   = string
+    }
+  )
 }
 
 variable "vmm_domain_dn" {
@@ -52,13 +37,13 @@ variable "l3_out" {
 # Use the provider to assigne credentials. We use these for everything going forward.
 provider "aci" {
   # This is the user name in ACI (usually a local users)
-  username = var.user_name
+  username = var.apic_auth.user_name
   # The private key should be in a file on the same system referenced by path
-  private_key = var.private_key
+  private_key = var.apic_auth.private_key
   # The cert name is the name assigned in ACI / Admin / AAA / Users / <User Name> / User Certificate
-  cert_name = var.cert_name
+  cert_name = var.apic_auth.cert_name
   # The URL for ACI
-  url      = "https://${var.apic}"
+  url      = "https://${var.apic_auth.apic}"
   insecure = true
 }
 
