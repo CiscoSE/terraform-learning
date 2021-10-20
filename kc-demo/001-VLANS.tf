f99d71df70c7e7486b10d8052b5a06bb413e1dc0
oldguardmd <oldguard@msn.com>
2021-10-20 22:03:55 +0000

    Modified VLAN pool name

--- kc-demo/001-VLANS.tf
+++ kc-demo/001-VLANS.tf
@@ -1,15 +1,15 @@
 resource "aci_physical_domain" "labsystems_physical_dom" {
     name = local.domain
-    relation_infra_rs_vlan_ns = aci_vlan_pool.labsystems_vlan_pool.id
+    relation_infra_rs_vlan_ns = aci_vlan_pool.kcdemo_vlan_pool.id
 }
 
-resource "aci_vlan_pool" "labsystems_vlan_pool" {
+resource "aci_vlan_pool" "kcdemo_vlan_pool" {
     name = local.vlan_pool
     alloc_mode = "static"
 }
 
 resource "aci_ranges" "demo1_vlan_range" {
-   vlan_pool_dn = aci_vlan_pool.labsystems_vlan_pool.id
+   vlan_pool_dn = aci_vlan_pool.kcdemo_vlan_pool.id
    from = "vlan-${local.vlan_number1}"
    to   = "vlan-${local.vlan_number1}"
    alloc_mode = "static"
@@ -17,7 +17,7 @@ resource "aci_ranges" "demo1_vlan_range" {
 }
 
 resource "aci_ranges" "demo2_vlan_range" {
-   vlan_pool_dn = aci_vlan_pool.labsystems_vlan_pool.id
+   vlan_pool_dn = aci_vlan_pool.kcdemo_vlan_pool.id
    from = "vlan-${local.vlan_number2}"
    to   = "vlan-${local.vlan_number2}"
    alloc_mode = "static"
