b2841e6e04dafa03bcf9f1495c1584d8844a1fd6
oldguardmd <oldguard@msn.com>
2021-10-20 12:39:46 -0400

    Removed renamed files

--- kc-demo/004-application.tf
+++
@@ -1,5 +0,0 @@
-resource "aci_application_profile" "t1a1" {
-    name                = "t1a1-Demo"
-    tenant_dn           = aci_tenant.tenant1.id
-}
-
--- kc-demo/005-Tenants.tf
+++
@@ -1,3 +0,0 @@
-resource "aci_tenant" "tenant1" {
-  name = local.tenant_name
-}
