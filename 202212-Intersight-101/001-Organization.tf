d37afc549e8621e44322e628e6b9bc9d2218fd5e
oldguardmd <oldguard@msn.com>
2022-12-02 13:29:09 +0000

    Working Basic Example of Auth and a few policies

---
+++ 202212-Intersight-101/001-Organization.tf
@@ -0,0 +1,13 @@
+# We could create this here, but we assume you already have this
+
+# This reference is used to to give us the moid of the organization based on the name.
+data "intersight_organization_organization" "org1" {
+    name = var.domain.org_name
+}
+
+# Results you can get from this include:
+#       name
+#       create_time
+#       description
+#       mod_time
+#       moid    <-- This is the one we use in this example
