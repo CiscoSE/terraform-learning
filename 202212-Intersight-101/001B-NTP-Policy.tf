5a7ec9b7e0f8c396896bf94654c708b0bd07eef2
oldguardmd <oldguard@msn.com>
2022-12-05 12:46:44 +0000

    New Naming Strategy

---
+++ 202212-Intersight-101/001B-NTP-Policy.tf
@@ -0,0 +1,14 @@
+resource "intersight_ntp_policy" "ntp1" {
+  name    = var.domain.ntp_policy_name
+  enabled = true
+  ntp_servers = [var.domain.ntp1]
+  timezone = var.domain.time_zone
+  organization {
+    moid   = data.intersight_organization_organization.org1.id
+    object_type = "organization.Organization"
+  }
+  tags {
+    key   = var.domain.tag_key1
+    value = var.domain.tag_value1
+  }
+}
