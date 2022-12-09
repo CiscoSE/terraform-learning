83beed4f1a5a3c7c6aa837e4bf438af97de40a73
oldguardmd <oldguard@msn.com>
2022-12-09 18:09:56 +0000

    Moving to private repo

--- 202212-Intersight-101/001A-Organization.tf
+++
@@ -1,6 +0,0 @@
-# We could create this here, but we assume you already have this
-
-# This reference is used to to give us the moid of the organization based on the name.
-data "intersight_organization_organization" "org1" {
-    name = var.domain.org_name
-}
